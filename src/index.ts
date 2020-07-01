4e310fee8d99e86d2cd19d703099f57a38774455
Nicolas <nicolas.escouto@gmail.com>
2020-07-01 13:05:19 -0300

    basic front-end

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import express from 'express'
+const cors = require('cors')
 import Router from './application/http/routes/Router'
 import sequelize from './infrastructure/database/sequelize'
+import serveStatic from 'serve-static'
+import path from 'path'
+
 
 const app = express()
 
 sequelize.start()
 
+app.use(cors())
 app.use(express.json())
 app.use(Router)
+
+
+app.use(serveStatic(path.join(__dirname, '../front')))
 
 app.listen(3000, () => {
     console.log('Servidor iniciado.')
